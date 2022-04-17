9eae8767076f193e4133ca3d14fe4d1e1f07a9ab
Andrey1297 <84615172+Andrey1297@users.noreply.github.com>
2022-04-17 08:38:08 -0200

    Update game.py

--- game.py
+++ game.py
@@ -14,6 +14,20 @@ class GameSprite(sprite.Sprite):
 
         def reset(self):
             window.blit(self.image, (self.rect.x, self.rect.y))
+        
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed() 
+        if keys[K_UP] and self.rect.y > 5: 
+            self.speed self.rect.y 
+        if keys [K_DOWN] and self.rect .y < win_height-80: 
+            self.rect.y += self.speed 
+    def update_l(self): 
+        key.get_pressed() 
+        if keys [K_w] and self.rect.y > 5: 
+            self.rect.y -= self.speed
+        if keys [K_s] and self .rect.y < win_height -80:
+            self.rect.y += self.speed
 #2
 #3
 back = (200,255,255)
